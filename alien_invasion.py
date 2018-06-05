683395d3d6de58240461d0e31e403280e33831ec
Caleb <rogersjcaleb@gmail.com>
2018-06-04 20:35:41 -0700

    Move event loop stuff to game functions file

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game, settings, and create a screen object.
@@ -19,11 +20,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
+        gf.check_events()
 
-        # Watch for keybord and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
